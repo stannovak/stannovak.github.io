8801f90848001c45c37d63825c708180275953d3
Stanislav Novak <novak.stan@gmail.com>
2015-12-02 20:49:25 +0200

    removed random url from main iframe

--- widget/impf_widget.js
+++ widget/impf_widget.js
@@ -70,7 +70,7 @@ var impfLoader = {
         iframe.height = cHeight;
         iframe.style.border = 'none';
         iframe.id = '__impf_widget_frame';
-        iframe.src = 'impf_widget_' + this._oid + '.html?tm=' + Math.random();
+        iframe.src = 'impf_widget_' + this._oid + '.html';
         this._container.appendChild(iframe);
         iframe.onload = function() {
             console.log('frame loaded');
